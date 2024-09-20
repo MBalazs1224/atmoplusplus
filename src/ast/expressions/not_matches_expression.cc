d4ac82e3a9453159f4cfc36cefd6269ec1d50505
MBazsi94 <balazsmolnar1224@gmail.com>
2024-09-20 13:38:06 +0200

    Implemented not matches expression checking, and fixed it's operator precedence, but I think it still needs work later

--- src/ast/expressions/not_matches_expression.cc
+++ src/ast/expressions/not_matches_expression.cc
@@ -10,6 +10,18 @@ std::shared_ptr<Type> NotMatchesExpression::GetType()
 
 bool NotMatchesExpression::Check() 
 {
-    // TODO: Implement Not matches expression checking
+    if (!exp_left->Check() || !exp_right->Check())
+    {
+        return false;
+    }
+    auto exp_left_type = exp_left->GetType();
+    auto exp_right_type = exp_right->GetType();
+
+    if (exp_left_type->NotEquals(exp_right_type))
+    {
+        Error::ShowError(Helper::FormatString("The two operands of NOT MATCHES (!=) expression must be of same type! (received '%s' and '%s')", exp_left_type->ToString().c_str(),exp_right_type->ToString().c_str()),this->location);
+        return false;
+    }
+
     return true;
 }
