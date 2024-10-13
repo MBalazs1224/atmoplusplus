141f1eb9aeadd1923866d0e9b65b95f9c9e472b1
MBalazs1224 <balazsmolnar1224@gmail.com>
2024-10-13 12:29:51 +0200

    Fixed reorder issue in function symbol constructors

--- src/symboltable/symbols/symbolfunction.cc
+++ src/symboltable/symbols/symbolfunction.cc
@@ -1,9 +1,9 @@
 #include "symbolfunction.hh"
 
 FunctionSymbol::FunctionSymbol(std::shared_ptr<Type> type_in, std::shared_ptr<Attribute> attr_in, std::vector<std::shared_ptr<VariableSymbol>> args_in, std::unique_ptr<BodyNode> body_in)
-    : SymbolTableElement(type_in, std::move(attr_in)),body(std::move(body_in)), arguments(std::move(args_in))  {}
+    : SymbolTableElement(type_in, std::move(attr_in)), arguments(std::move(args_in)),body(std::move(body_in))  {}
 
-FunctionSymbol::FunctionSymbol(std::shared_ptr<Attribute> attr_in, std::unique_ptr<BodyNode> body_in, std::vector<std::shared_ptr<VariableSymbol>> args_in) : SymbolTableElement(std::move(attr_in)), body(std::move(body_in)), arguments(std::move(args_in)) {}
+FunctionSymbol::FunctionSymbol(std::shared_ptr<Attribute> attr_in, std::unique_ptr<BodyNode> body_in, std::vector<std::shared_ptr<VariableSymbol>> args_in) : SymbolTableElement(std::move(attr_in)), arguments(std::move(args_in)), body(std::move(body_in)) {}
 
 
 std::shared_ptr<Type> FunctionSymbol::GetType() {
@@ -22,4 +22,4 @@ const std::vector<std::shared_ptr<VariableSymbol>>& FunctionSymbol::GetArguments
     return arguments;
 }
 
-FunctionSymbol::FunctionSymbol() : SymbolTableElement(std::make_shared<AttributePublic>()), body(nullptr), arguments() {}
+FunctionSymbol::FunctionSymbol() : SymbolTableElement(std::make_shared<AttributePublic>()), arguments() , body(nullptr) {}
