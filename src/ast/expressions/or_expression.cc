d098a16b4f895046fb096d52ab6ebecf45fd66d1
MBalazs1224 <balazsmolnar1224@gmail.com>
2025-01-15 17:42:16 +0100

    Implemented translating OR expression into IR

--- src/ast/expressions/or_expression.cc
+++ src/ast/expressions/or_expression.cc
@@ -33,12 +33,80 @@ bool OrExpression::Check() {
 
 std::shared_ptr<TranslateExpression> OrExpression::TranslateExpressionToIr()
 {
-    //TODO: "Implement or expression to ir"
-    return nullptr;
+    // Create the labels
+    auto trueLabel = std::make_shared<Label>();
+    auto falseLabel = std::make_shared<Label>();
+    auto joinLabel = std::make_shared<Label>();
+
+    // Translate the left and right expressions into IR
+
+    auto leftExp = left->TranslateExpressionToIr()->ToConditionExpression(trueLabel, falseLabel);
+
+    auto rightExp = right->TranslateExpressionToIr()->ToConditionExpression(trueLabel, falseLabel);
+
+    // Create the register that will be used to store the final value
+
+    auto reg = std::make_shared<Temp>();
+
+    // Statement to move 1 into the register
+    
+    auto move1IntoReg = std::make_shared<IRMove>(
+        std::make_shared<IRTemp>(reg),
+        std::make_shared<IRConst>(1)
+    );
+
+    // Statement to move 0 into the register
+
+    auto move0IntoReg = std::make_shared<IRMove>(
+        std::make_shared<IRTemp>(reg),
+        std::make_shared<IRConst>(0)
+    );
+
+    // If the first expression is true, set the reg to 1 and jump to the join label because of short circuiting
+
+    auto trueSequence = std::make_shared<IRSequence>(
+        std::make_shared<IRLabel>(trueLabel),
+        move1IntoReg,
+        std::make_shared<IRJump>(joinLabel)
+    );
+
+
+    // If the first expression is false, check the second expression
+
+    // FIXME: The false label might need to be moved after the rightexp or the tight exp might not need to jump tp the false label because it might create an infinite loop
+
+    auto falseSequence = std::make_shared<IRSequence>(
+        std::make_shared<IRLabel>(falseLabel),
+        std::make_shared<IRSequence>(
+            rightExp,
+            std::make_shared<IRSequence>(
+                move0IntoReg,
+                std::make_shared<IRJump>(joinLabel)
+            )
+        )
+    );
+
+    // Combine the true and false paths
+
+    auto finalSequence = std::make_shared<IRSequence>(
+        leftExp,
+        std::make_shared<IRSequence>(
+            trueSequence,
+            std::make_shared<IRSequence>(
+                falseSequence,
+                std::make_shared<IRLabel>(joinLabel)
+            )
+        )
+    );
+
+    // Execute the final sequence and get the reg as the final value
+    auto finalSeq = std::make_shared<IREseq>(finalSequence, reg);
+
+    return std::make_shared<TranslateExpression>(finalSeq);
+    
 }
 
 std::shared_ptr<IRStatement> OrExpression::TranslateToIR()
 {
-    //TODO: "Implement OrExpression to ir"
-    return nullptr;
+    throw std::logic_error("OrExpression::TranslateToIR() should not be called!");
 }
