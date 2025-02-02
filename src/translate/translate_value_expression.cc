4e2dbee821362d679ed196780a865c8696c473a7
MBalazs1224 <balazsmolnar1224@gmail.com>
2025-02-02 13:50:25 +0100

    Changed the translating value expressions that it now compares the value to 1 instead to 0 for easier readibility

--- src/translate/translate_value_expression.cc
+++ src/translate/translate_value_expression.cc
@@ -19,11 +19,11 @@ std::shared_ptr<IRStatement> TranslateValueExpression::ToNoValueExpression()
 
 std::shared_ptr<IRStatement> TranslateValueExpression::ToConditionExpression(std::shared_ptr<Label> trueLabel, std::shared_ptr<Label> flaseLabel)
 {
-    // To convert a value expression to a condition expression we need to compare it to 0 (if it not equals no 0, than it means the value is true, otherwise false)
+    // To convert a value expression to a condition expression we need to compare it to 1 (if equals means true, otherwise false)
 
-    auto zeroExpression = std::make_shared<IRConst>(0);
+    auto zeroExpression = std::make_shared<IRConst>(1);
 
-    auto condition = std::make_shared<IRCJump>(RelationalOperator::NotMatches, 
+    auto condition = std::make_shared<IRCJump>(RelationalOperator::Matches, 
         exp, 
         zeroExpression, 
         trueLabel, 
